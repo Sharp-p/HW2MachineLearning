641f2c7a28269e314896717800955e3b9062433f
Sharp-p <thomassharp2003@gmail.com>
2025-12-28 11:17:24 +0100

    feat: CNN structure

--- src/cnn.py
+++ src/cnn.py
@@ -10,6 +10,19 @@ class CNNModel:
         self.output_dim = output_dim
         self.model = models.Sequential()
 
+        # filter=32 i.e. the number of kernels applied in each convolutional layer
+        # input shape, we will experiment with size from 64x64 to 256x256 pixels
         self.model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=self.input_shape))
+        self.model.add(layers.MaxPooling2D(pool_size=(3, 3)))
+        self.model.add(layers.Conv2D(32, (3, 3), activation='relu'))
+        self.model.add(layers.MaxPooling2D(pool_size=(3, 3)))
+        self.model.add(layers.Conv2D(32, (3, 3), activation='relu'))
+        self.model.add(layers.MaxPooling2D(pool_size=(3, 3)))
+        self.model.add(layers.Conv2D(32, (3, 3), activation='relu'))
 
+        self.model.add(layers.Flatten())
+        self.model.add(layers.Dense(32, activation='relu'))
+        self.model.add(layers.Dense(64, activation='relu'))
+        self.model.add(layers.Dense(self.output_dim, activation='softmax'))
 
+        #TODO: copilare modello
